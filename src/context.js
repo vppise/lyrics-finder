a6bf619446160e5ffdf06b119209d244d047cbb9
vikas <vikasppise@gmail.com>
2022-05-06 14:22:13 +0530

    api key added

--- src/context.js
+++ src/context.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios'
- 
+
 const Context = React.createContext();
 
 const reducer = (state, action) => {
-    switch(action.type){
+    switch (action.type) {
 
         case 'SEARCH_TRACKS':
-            return{
+            return {
                 ...state,
                 track_list: action.payload,
                 heading: 'Search Result'
             };
-            default: 
+        default:
             return state;
 
     }
 }
 
 export class Provider extends Component {
-    
+
     state = {
         track_list: [],
         heading: 'Top 10 Tracks',
@@ -27,18 +27,19 @@ export class Provider extends Component {
     }
 
     componentDidMount() {
-        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
-        .then(res => {
-            console.log(res.data)
-            this.setState({track_list: res.data.message.body.track_list});
-        })
-        .catch(err => console.log(err));
+        // axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+        axios.get(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+            .then(res => {
+                console.log(res.data)
+                this.setState({ track_list: res.data.message.body.track_list });
+            })
+            .catch(err => console.log(err));
     }
-    
+
     render() {
         return (
             <Context.Provider value={this.state}>
-              {this.props.children}  
+                {this.props.children}
             </Context.Provider>
         )
     }
